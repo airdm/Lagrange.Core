4d73806e670f00f199b7399c0b5b352ab1d711f7
airdm <air@airdm.com>
2025-07-14 02:33:51 +0800

    Update PushMessageService.cs

--- Lagrange.Core/Internal/Service/Message/PushMessageService.cs
+++ Lagrange.Core/Internal/Service/Message/PushMessageService.cs
@@ -286,9 +286,10 @@ internal class PushMessageService : BaseService<PushMessageEvent>
                 
                 if (Serializer.Deserialize<FriendRequest>(content.AsSpan()).Info is { } info)
                 {
-                    Collection.Log.LogInfo(Tag, $"ceshi to {info}");
+                    _logger.LogInformation("ceshi: {}", info);
                     var friendEvent = FriendSysRequestEvent.Result(msg.Message.ResponseHead.FromUin, info.SourceUid, info.Message, info.Source ?? info.NewSource);
-                    Collection.Log.LogInfo(Tag, $"ceshi2 to {friendEvent}");
+                    
+                    _logger.LogInformation("ceshi2: {}", friendEvent);
                     extraEvents.Add(friendEvent);
                 }
                 break;
